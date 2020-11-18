6e3c059e29ed17ff6bd34d86729dbde529f829f4
Javier Granados <contact@jvgranados.com>
2020-11-18 15:16:29 +0100

    Add purgecss and stylelint

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,11 @@
+const purgecss = [
+  '@fullhuman/postcss-purgecss',
+  {
+    content: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
+    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+  },
+]
+
 module.exports = {
-  plugins: ['tailwindcss', 'postcss-preset-env'],
+  plugins: ['tailwindcss', process.env.NODE_ENV === 'production' ? purgecss : undefined, 'postcss-preset-env'],
 }
